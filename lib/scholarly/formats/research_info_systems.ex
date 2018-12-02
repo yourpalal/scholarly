eadc9eae014735327742759df46af4190ce943d4
Alex Wilson <aewilson@ualberta.ca>
2018-12-01 19:49:43 -0500

    Add support for RIS A1/A2/A3/A4 fields
     * support numbered author fields
     * move RIS tests to their own file

--- lib/scholarly/formats/research_info_systems.ex
+++ lib/scholarly/formats/research_info_systems.ex
@@ -1,8 +1,8 @@
 defmodule Scholarly.ResearchInfoSystems do
   # https://en.wikipedia.org/wiki/RIS_(file_format)
 
-  @split_pattern ~r/(?<linebreak>\r?\n)[A-Z][A-Z]  -/sm
-  @field_pattern ~r/\A(?<key>[A-Z][A-Z])  - (?<value>.*)/sm
+  @split_pattern ~r/(?<linebreak>\r?\n)[A-Z][A-Z0-9]  -/sm
+  @field_pattern ~r/\A(?<key>[A-Z][A-Z0-9])  - (?<value>.*)/sm
 
   def parse(input) when is_binary(input) do
     records = Regex.split(@split_pattern, input, on: [:linebreak])
@@ -29,7 +29,19 @@ defmodule Scholarly.ResearchInfoSystems do
   end
 
   defp rows_to_record(rows) do
-    Enum.reduce(rows, %Scholarly.Record{}, &add_field/2)
+    starter = %{
+      record: %Scholarly.Record{},
+      authors: %{"1": nil, "2": nil, "3": nil, "4": nil, from: [] }
+    }
+    filled = Enum.reduce(rows, starter, &add_field/2)
+
+    authors = ~w(4 3 2 1)a
+              |> Enum.reduce(Enum.reverse(filled[:authors][:from]),
+                    fn k, acc -> [filled[:authors][k] | acc] end)
+              |> Enum.reject(&is_nil/1)
+
+    record = filled[:record]
+    %{record | authors: authors}
   end
 
   defp split_field(input) do
@@ -38,47 +50,70 @@ defmodule Scholarly.ResearchInfoSystems do
   end
 
   defp add_field({"TY", value}, record) do
-    %{record | type: value}
+    update_simple record, :type, value
   end
 
   defp add_field({"TI", value}, record) do
-    %{record | title: value}
+    update_simple record, :title, value
   end
 
   defp add_field({"AB", value}, record) do
-    %{record | abstract: value}
+    update_simple record, :abstract, value
   end
 
   defp add_field({"JO", value}, record) do
-    %{record | journal: value}
+    update_simple record, :journal, value
   end
 
   defp add_field({"ID", value}, record) do
-    %{record | journal_assigned_id: value}
+    update_simple record, :journal_assigned_id, value
   end
 
   defp add_field({"AU", value}, record) do
-    %{record | authors: record.authors ++ [value]}
+    update_in record, [:authors, :from], &([value | &1])
+  end
+
+  defp add_field({"A1", value}, record) do
+    put_in record, [:authors, :"1"], value
+  end
+
+  defp add_field({"A2", value}, record) do
+    put_in record, [:authors, :"2"], value
+  end
+
+  defp add_field({"A3", value}, record) do
+    put_in record, [:authors, :"3"], value
+  end
+
+  defp add_field({"A4", value}, record) do
+    put_in record, [:authors, :"4"], value
   end
 
   defp add_field({"PY", value}, record) do
-    %{record | published_year: value}
+    update_simple record, :published_year, value
   end
 
   defp add_field({"DA", value}, record) do
     [y, m, d] = String.split(value, "/")
-    %{record | published_year: y, published_month: m, published_day: d}
+    record
+    |> update_simple(:published_year, y)
+    |> update_simple(:published_month, m)
+    |> update_simple(:published_day, d)
   end
 
   defp add_field({"SN", value}, record) do
-    %{record | standard_number: value}
+    update_simple record, :standard_number, value
   end
 
   defp add_field({"UR", value}, record) do
-    %{record | url: value}
+    update_simple record, :url, value
   end
 
   defp add_field({"DO", value}, record) do
-    %{record | doi: value}
+    update_simple record, :doi, value
+  end
+
+  defp update_simple(record, field, value) do
+    %{record | record: %{record.record | field => value}}
   end
 end
